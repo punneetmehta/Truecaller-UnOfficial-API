c0a25a88b1c6e5078e739768961887299bca6212
Puneet Mehta <mailme.puneetmehta@gmail.com>
2016-06-01 19:58:14 +0530

    Create truesearch.py

---
+++ python/truesearch.py
@@ -0,0 +1,19 @@
+import urllib2
+
+no = "" # Any Number You Want to Search
+
+request_headers = {
+"X-User": "PHPHive"
+}
+
+# For Searching User Details
+print "Searching for "+no;
+request = urllib2.Request("https://tcapi.phphive.info/search/"+no, headers=request_headers)
+contents = urllib2.urlopen(request).read()
+print contents
+
+# For Searching Profile Picture
+print "Getting Profile Picture...";
+request = urllib2.Request("https://tcapi.phphive.info/pic/"+no, headers=request_headers)
+contents = urllib2.urlopen(request).read()
+print contents;
