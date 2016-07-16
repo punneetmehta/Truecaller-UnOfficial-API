d9749fbc782ccd0f037e94c9cd56e755be7f3626
Puneet Mehta <mailme.puneetmehta@gmail.com>
2016-07-16 10:57:47 +0530

    Update truesearch.php

--- php/truesearch.php
+++ php/truesearch.php
@@ -3,30 +3,11 @@
 
 $no = ""; // Any Number You want to Search 
 
-search($no); // echo Details like Name, Internet Address ( including Email, Facebook & Twitter ), Spam Score, Spammer Type etc.
-
-searchProfilePic($no); // echo ProfilePic Url ( if Available on TrueCaller )
+search($no); // echo Details like Name, Internet Address ( including Profile Picutre, Email, Facebook & Twitter ), Spam Score, Spammer Type etc.
 
 function search($no){
 
 $uri = 'https://tcapi.phphive.info/search/'.$no;
-$ch = curl_init($uri);
-curl_setopt_array($ch, array(
-    CURLOPT_HTTPHEADER  => array('X-User: PHPHive'),
-    CURLOPT_RETURNTRANSFER  =>true,
- CURLOPT_SSL_VERIFYHOST => false,
- CURLOPT_SSL_VERIFYPEER => false,
-    CURLOPT_VERBOSE     => 1
-));
-$response = curl_exec($ch);
-curl_close($ch);
-echo $response;
-
-}
-
-function searchProfilePic($no){
-
-$uri = 'https://tcapi.phphive.info/pic/'.$no;
 $ch = curl_init($uri);
 curl_setopt_array($ch, array(
     CURLOPT_HTTPHEADER  => array('X-User: PHPHive'),
