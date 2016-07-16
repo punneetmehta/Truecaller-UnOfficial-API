67bba34541cda7450983db9dff38f5516bd48a9d
Puneet Mehta <mailme.puneetmehta@gmail.com>
2016-07-16 10:58:33 +0530

    Update truesearch.java

--- java/truesearch.java
+++ java/truesearch.java
@@ -30,8 +30,7 @@ public class truesearch {
         	System.out.println("Please Enter a Number on Line 28");
         	return ;
         }
-		tcapi.search(no); // prints Details like Name, Internet Address ( including Email, Facebook & Twitter ), Spam Score, Spammer Type etc.
-		tcapi.searchProfilePic(no); // prints ProfilePic Url ( if Available on TrueCaller )
+		tcapi.search(no); // prints Details like Profile Picture, Name, Internet Address ( including Email, Facebook & Twitter ), Spam Score, Spammer Type etc.
         } catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -69,38 +68,6 @@ public class truesearch {
 
 	}
 
-	private void searchProfilePic(String no) throws Exception {
-
-		String url = "https://tcapi.phphive.info/pic/"+no;
-		URL obj = new URL(url);
-		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-        // For Handling SSL Handshakes
-        if( null == sslSocketFactory) {
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, ALL_TRUSTING_TRUST_MANAGER, new java.security.SecureRandom());
-            sslSocketFactory = sc.getSocketFactory();
-        }
-        con.setSSLSocketFactory(sslSocketFactory);
-        con.setHostnameVerifier(ALL_TRUSTING_HOSTNAME_VERIFIER);
-		//add reuqest header
-		con.setRequestMethod("GET");
-		con.setRequestProperty("X-User", "PHPHive");
-		int responseCode = con.getResponseCode();
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-		
-		//print result
-		System.out.println(response.toString());
-
-	}
-	
 	// For Handling SSL Handshakes
     private static final TrustManager[] ALL_TRUSTING_TRUST_MANAGER = new TrustManager[] {
             new X509TrustManager() {
