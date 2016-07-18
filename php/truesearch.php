100283c685944b686701aaa619f968494dc020fe
Puneet Mehta <mailme.puneetmehta@gmail.com>
2016-07-18 12:52:40 +0530

    Update truesearch.php

--- php/truesearch.php
+++ php/truesearch.php
@@ -1,5 +1,6 @@
 <?PHP
 
+$APIToken = ""; // PHPHive Truecaller API Token, Obtain it from https://tcapi.phphive.info/console/
 
 $no = ""; // Any Number You want to Search 
 
@@ -7,7 +8,7 @@ search($no); // echo Details like Name, Internet Address ( including Profile Pic
 
 function search($no){
 
-$uri = 'https://tcapi.phphive.info/search/'.$no;
+$uri = 'https://tcapi.phphive.info/'.$APIToken.'/search/'.$no;
 $ch = curl_init($uri);
 curl_setopt_array($ch, array(
     CURLOPT_HTTPHEADER  => array('X-User: PHPHive'),
